af4e1b08e4aa18aea3cfb60f4ceb2fcd0a090aa3
kasturimohan <mohanreddyxworkz@gmail.com>
2021-09-24 00:10:23 -0700

    first file upload

---
+++ CarRunner.java
@@ -0,0 +1,16 @@
+class Name {
+     static void bike(){
+ System.out.println("pulsar");
+ }
+ static void bike (String name) {
+System.out.println(name);
+} 
+}
+class CarRunner
+	{
+	public static void main(String[] args)
+	{
+		Name.bike();
+		Name.bike("bullet");
+	} 
+}
